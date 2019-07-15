2c4052f53c5a0f327bc9785c5952e53d0c75589a
shmeliavCoder <ripper963@gmail.com>
2019-07-15 10:00:16 +0300

    Add files via upload

---
+++ ChainOfResp/BillHandle2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResp
+{
+    class BillHandle2 : BillHandlerBase
+    {
+        public override void Handle(int amount)
+        {
+            if (amount != 0)
+            {
+                if (amount >= 2)
+                {
+                    Console.WriteLine($"Giving 2 x {amount / 2} bills");
+                }
+
+                if (amount % 2 > 0)
+                {
+                    if (next != null)
+                    {
+                        next.Handle(amount % 2);
+                    }
+                }
+            }
+            
+        }
+    }
+}
