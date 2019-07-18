3873d2400e9ee2b8d40d16f629d456e43f979e8f
shmeliavCoder <ripper963@gmail.com>
2019-07-18 10:04:51 +0300

    Add files via upload

---
+++ Strategy/NumberCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class NumberCollection
+    {
+        public List<int> listOfNumbers { get; set; }
+        private ISort sortStrategy { get; set; }
+
+        public NumberCollection()
+        {
+            listOfNumbers = new List<int>();
+        }
+
+        public void Add(int number)
+        {
+            listOfNumbers.Add(number);
+
+            if (listOfNumbers.Count > 10)
+                sortStrategy = new MergeSort();
+            else
+                sortStrategy = new QuickSort();
+        }
+
+        public void Remove(int number)
+        {
+            if (listOfNumbers.Count > 10)
+                sortStrategy = new MergeSort();
+            else
+                sortStrategy = new QuickSort();
+        }
+
+        public void Sort()
+        {
+            sortStrategy.Sort(listOfNumbers);
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberCollection nc = new NumberCollection();
+            nc.Add(1);
+            nc.Add(2);
+            nc.Add(3);
+            nc.Add(4);
+            nc.Add(5);
+            nc.Add(6);
+            nc.Add(7);
+            nc.Add(8);
+            nc.Add(9);
+
+            nc.Sort();
+
+            nc.Add(10);
+            nc.Add(11);
+            nc.Sort();
+        }
+    }
+}
